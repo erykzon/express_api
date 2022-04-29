8ad341884de8af0a61551d726769f0e1b8d549a4
Erykzon <jota.once6mail.com>
2022-04-28 19:40:02 -0500

    Paso 7 Agregando un nuevo endpoint con metodo PUT

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ app.post('/v1/explorers', (req, res) =>{
     res.status(201).json({message: "Created"})
 
 })
+//PUT Crea un endpoint que se encargue de actualizar un explorer
+app.put('/v1/explorers/:id', (req, res) =>{
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with is ${req.params.id}`)
+    const requestBody =req.body// Parametros de un cliente
+    res.status(200).json({message: "Updated!"})
+
+})
 
 
 // Con esto inicializamos esta app
